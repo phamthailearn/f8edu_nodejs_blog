3919239ea6aa0c44aa871daf60e23860d35b6843
phamthaicoder <phamthaicoder@gmail.com>
2022-03-09 09:56:59 +0700

    update model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,6 +1,17 @@
+const Course = require('../models/Course');
+
 class SiteController {
     index(req, res) {
-        res.render('home');
+        // res.json({
+        //     name: 'test'
+        // });
+        // res.render('home');
+        Course.find({}, function(err, courses){
+            if(!err) {
+                res.json(courses);
+            } 
+            else res.status(400).json({error: 'Error!'});
+        })
     }
 
     search(req, res) {
